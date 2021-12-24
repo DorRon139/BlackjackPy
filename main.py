4d30a4c50e1dbc6bd3baf52cb02f8ca13e134864
Dor <dorron139@gmail.com>
2021-12-24 22:11:38 +0200

    Commit check 2

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from participants import Player
 
 p = Player('Dor', 100)
 p2 = Player('Shaked', 100)
-
-g = Game([p])
+p3 = Player('Inbar', 1000)
+g = Game([p,p3])
 
 while len(g.players) > 0:
     g.initialize_game()
